5862be539d26855507e2d3c4305c5d2bc02ee6d5
Sujitha K Y <sujitaky@gmail.com>
2021-06-08 12:51:44 +0530

    commit

--- areaOfTriangle.py
+++ areaOfTriangle.py
@@ -1,5 +1,5 @@
 # finding area of triangle
 height=int(input("Enter the height of the triangle: "))
 base=int(input("Enter the base of the triangle: "))
-area=0.5*base*height
+area=(1/2)*base*height
 print("The area of triangle is ",area)
