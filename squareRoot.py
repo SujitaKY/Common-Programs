3f1d6651bff40e90ac5a31815e990e3e8e71f989
Sujitha K Y <sujitaky@gmail.com>
2021-06-08 12:54:21 +0530

    commit

--- squareRoot.py
+++ squareRoot.py
@@ -3,3 +3,5 @@
 num=int(input("Enter a number: "))
 sqrt=num**0.5    #  ** represents the right is the power of left i.e if 81**05=81 power 0.5 which gives the root of 81 = 9
 print("The square root of %d is %0.2f"%(num,sqrt))
+
+#there is a in-built module called cmath for using sqrt in-built function
